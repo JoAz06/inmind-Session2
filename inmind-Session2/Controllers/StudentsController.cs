d9e611ba9e3c6f3d35ccbaa5c0afd882e4184554
Joseph <Josephazzi2006@gmail.com>
2026-02-07 17:50:55 +0200

    Step 7

--- inmind-Session2/Controllers/StudentsController.cs
+++ inmind-Session2/Controllers/StudentsController.cs
@@ -11,7 +11,10 @@ public class StudentsController : Controller
     public static List<Student> ListOfStudents = new()
     {
         new Student(1, "John Smith"),
-        new Student(2, "Jane Doh")
+        new Student(2, "John Baker"),
+        new Student(3, "John Doh"),
+        new Student(4, "John Naples"),
+        new Student(5, "Jane Doh")
     };
 
     [HttpGet]
@@ -29,6 +32,17 @@ public class StudentsController : Controller
             return Ok(student);
         }
         return NotFound();
+    }
+
+    [HttpGet("search")]
+    public ActionResult<List<Student>> SearchStudents([FromQuery] string name)
+    {
+        if(string.IsNullOrEmpty(name))
+            return BadRequest("Name cant be  null or empty");
+        List<Student> filteredStudents = ListOfStudents.Where(x => x.name.ToLower().Contains(name.ToLower())).ToList();
+        if (filteredStudents.Count == 0)
+            return NotFound();
+        return Ok(filteredStudents);
     }
 
 }
